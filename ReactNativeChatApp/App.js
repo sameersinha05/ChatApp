ae0bdcdd894e01b7b0d7ae12def907cf6e887460
sameersinha05 <53221740+sameersinha05@users.noreply.github.com>
2019-08-18 22:56:36 +0530

    Update App.js

--- ReactNativeChatApp/App.js
+++ ReactNativeChatApp/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Chat from './Chat';
-import ChatScreenComponent from './Chat/ChatScreen/Component';
+import React, {Component} from 'react'
+import { StyleSheet } from 'react-native';
+import Root from './Root';
 
-export default function App() {
-  return (
-    <ChatScreenComponent />
-  );
+export default class App extends Component {
+
+  render() {
+    return (
+      <Root />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
